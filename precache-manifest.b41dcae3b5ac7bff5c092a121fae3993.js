f9565701f16baa80b6d3c907f1d59a06c42f7935
KTTITO <by.kttito@gmail.com>
2019-05-23 15:17:29 -0500

    presupuestp

---
+++ precache-manifest.b41dcae3b5ac7bff5c092a121fae3993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f91fb61b2ac49a9dc810e7cb1aa5769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2092e93d1a832d454221",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "2092e93d1a832d454221",
+    "url": "/static/js/main.398a3718.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
